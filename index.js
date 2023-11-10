4e9c3a37759becd4ce27c16513424402f871e0f9
muaz <muazibrahim.95@outlook.com>
2023-11-10 16:03:04 -0600

    multiple operations use and decimal use done

--- index.js
+++ index.js
@@ -25,112 +25,135 @@ const operate = (num1, operator, num2) => {
     }
 };
 
-const btn = document.querySelectorAll('button');
 const display = document.getElementById('output');
 const numbersBtn = document.querySelectorAll('.button.numbers');
 const operatorBtn =document.querySelectorAll('.button.operators');
 const equalsBtn = document.getElementById('solve');
 const clearBtn = document.getElementById('clear');
+const decimalBtn = document.getElementById('decimal')
 
 let isFirstClick = true;
+let firstDecimal = true;
 
 numbersBtn.forEach(numbers => {
     numbers.addEventListener('click', () => {
-         // Check if it's the first click
-         if (isFirstClick) {
-            // Clear the initial text content
+        if (isFirstClick) {
             display.textContent = '';
-            // Update the flag to false for subsequent clicks
             isFirstClick = false;
-        };
+            firstDecimal = true; // Allow adding a decimal to the new number
+        }
 
         display.textContent += numbers.textContent;
-    })
+    });
 });
 
 operatorBtn.forEach(symbol => {
     symbol.addEventListener('click', () => {
-
-        if(symbol.textContent === '+'){
-            // store number, operator and clear display
-            num1 = Number(display.textContent);
-            operator = '+';
-            display.textContent = '';
-        };
-
-        if(symbol.textContent === '-'){
-            // store number, operator and clear display
-            num1 = Number(display.textContent);
-            operator = '-';
-            display.textContent = '';
-        };
-
-        if(symbol.textContent === 'x'){
-            // store number, operator and clear display
-            num1 = Number(display.textContent);
-            operator = 'x';
-            display.textContent = '';
-        };
-
-        if(symbol.textContent === '÷'){
-            // store number, operator and clear display
-            num1 = Number(display.textContent);
-            operator = '÷';
-            display.textContent = '';
-        };
-    
+        if (!isFirstClick) {
+            if (operator !== '') {
+                num2 = Number(display.textContent);
+                display.textContent = operate(num1, operator, num2);
+                num1 = Number(display.textContent);
+            } else {
+                num1 = Number(display.textContent);
+            }
+        }
+        isFirstClick = true;
+        operator = symbol.textContent;
+        firstDecimal = true; // Reset firstDecimal whenever an operator is clicked
     });
 });
 
-equalsBtn.addEventListener('click', () =>{
-    num2 = Number(display.textContent);
-    display.textContent = operate(num1, operator, num2);
+decimalBtn.addEventListener('click', () => {
+    if (display.textContent !== '' && firstDecimal) {
+        display.textContent += '.';
+        firstDecimal = false;
+    }
+});
 
+equalsBtn.addEventListener('click', () => {
+    if (!isFirstClick) {
+        num2 = Number(display.textContent);
+        display.textContent = operate(num1, operator, num2);
+        isFirstClick = true;
+        firstDecimal = true; // Reset firstDecimal after calculation
+    }
 });
 
 clearBtn.addEventListener('click', () => {
     num1 = '';
     num2 = '';
     operator = '';
-    display.textContent = 0;
+    firstDecimal = true;
+    display.textContent = '0';
     isFirstClick = true;
 });
 
-// // Add click event listeners to each button
-// btn.forEach(button => {
-//     button.addEventListener('click', () => {
-//         // Check if it's the first click
-//         if (isFirstClick) {
-//             // Clear the initial text content
-//             display.textContent = '';
-//             // Update the flag to false for subsequent clicks
-//             isFirstClick = false;
-//         };
-        
-//         const numbers = document.querySelectorAll('.button.numbers');
-//         numbers.forEach(n => console.log(n.textContent));
-//         // Append the text content of the clicked button to the display
-//         if(display.textContent in numbers.textContent){
-//             display.textContent += button.textContent;
-//         } 
-        
-//         if(button.textContent === '+'){
-//             // store numbers on display then clear display
+
+// operatorBtn.forEach(symbol => {
+//     symbol.addEventListener('click', () => {
+
+//         if(isFirstClickOperator && operator === ''){
+//         if(symbol.textContent === '+'){
+//             // store number, operator and clear display
 //             num1 = Number(display.textContent);
-//             display.textContent = '';
-//         }else if(button.textContent === '='){
-//             alert(display.textContent);
+//             operator = '+';
+//             isFirstClickOperator = false;
+//         };
 
+//         if(symbol.textContent === '-' && isFirstClickOperator){
+//             // store number, operator and clear display
+//             num1 = Number(display.textContent);
+//             operator = '-';
+//             isFirstClickOperator = false;
+//         };
+
+//         if(symbol.textContent === 'x' && isFirstClickOperator){
+//             // store number, operator and clear display
+//             num1 = Number(display.textContent);
+//             operator = 'x';
+//             isFirstClickOperator = false;
+//         };
+//         if(symbol.textContent === '÷' && isFirstClickOperator){
+//             // store number, operator and clear display
+//             num1 = Number(display.textContent);
+//             operator = '÷';
+//             isFirstClickOperator = false;
+//         };
+//         } else if(isFirstClickOperator == false){
 //             num2 = Number(display.textContent);
-//             display.textContent = add(num1, num2);
-//         } else if(button.textContent === 'AC'){
-//             display.textContent = 0;
-//             isFirstClick = true;
-//             num1 = 0;
-//             num2 = 0;
-//         }
-        
+//             display.textContent = operate(num1, operator, num2); 
+//             num1 = Number(display.textContent);  
+//             isFirstClickOperator = true;
+//         } else if(isFirstClickOperator == true && operator !== '') {
+//             if(symbol.textContent === '+'){
+//                 // store number, operator and clear display
+//                 num2 = Number(display.textContent);
+//                 operator = '+';
+//             };
+
+//             if(symbol.textContent === '-' && isFirstClickOperator){
+//                 // store number, operator and clear display
+//                 num2 = Number(display.textContent);
+//                 operator = '-';
+//                 isFirstClickOperator = false;
+//             };
+
+//             if(symbol.textContent === 'x' && isFirstClickOperator){
+//                 // store number, operator and clear display
+//                 num2 = Number(display.textContent);
+//                 operator = 'x';
+//                 isFirstClickOperator = false;
+//             };
+//             if(symbol.textContent === '÷' && isFirstClickOperator){
+//                 // store number, operator and clear display
+//                 num2 = Number(display.textContent);
+//                 operator = '÷';
+//                 isFirstClickOperator = false;
+//             };
+//         } 
+//     });
+// });
 
 
-//     })
-// })
+
